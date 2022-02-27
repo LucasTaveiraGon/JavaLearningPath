f5736f0fd5ba18fc00c235dd988c5f92ac7785d2
Lucas Taveira <lucastaveirag@gmail.com>
2022-02-27 17:49:13 -0300

    WorkerContract

---
+++ WorkerContract/src/entities/enums/WorkerLevel.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entities.enums;
+
+/**
+ *
+ * @author lucas
+ */
+public enum WorkerLevel {
+    JUNIOR,
+    MID_LEVEL,
+    SENIOR;
+}
