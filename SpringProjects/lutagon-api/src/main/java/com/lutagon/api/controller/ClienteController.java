1636aef7e724e8335195076380bb3de7764a87f6
Lucas Taveira <lucastaveirag@gmail.com>
2022-03-14 18:03:00 -0300

    updates exercises

--- SpringProjects/lutagon-api/src/main/java/com/lutagon/api/controller/ClienteController.java
+++ SpringProjects/lutagon-api/src/main/java/com/lutagon/api/controller/ClienteController.java
@@ -1,5 +1,6 @@
 package com.lutagon.api.controller;
 
+import java.util.Arrays;
 import java.util.List;
 
 import com.lutagon.domain.model.Cliente;
@@ -12,6 +13,23 @@ public class ClienteController {
 
 	@GetMapping("/clientes")
 		public List<Cliente>listar() {
-		return "Teste";
+			Cliente cliente1 = new Cliente();
+			cliente1.setId(1L);
+			cliente1.setNome("Joao");
+			cliente1.setTelefone("xxxxxxx-xxx");
+			cliente1.setEmail("xuxux@abrobrinha.com");
+
+			Cliente cliente2 = new Cliente();
+			cliente2.setId(2L);
+			cliente2.setNome("Sasha");
+			cliente2.setTelefone("xxxxxxx-xxx");
+			cliente2.setEmail("xaxaxa@abrobrinha.com");
+
+			Cliente cliente3 = new Cliente();
+			cliente3.setId(3L);
+			cliente3.setNome("SevenK");
+			cliente3.setTelefone("xxxxxxx-xxx");
+			cliente3.setEmail("xuxuddddddx@abrobrinha.com");
+		return Arrays.asList(cliente1, cliente2, cliente3);
 	}
 }
--- testechaosproject/src/App.java
+++ testechaosproject/src/App.java
@@ -1,84 +1,17 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
+		Scanner leitor = new Scanner(System.in);
+		DecimalFormat df = new DecimalFormat("0E00");    // Formato
 
-		double N;
-		int quociente, resto, nota, moeda;
-		N = sc.nextDouble();
-		resto = (int) (N * 100.0 + 0.5);
-		
-		System.out.println("NOTAS:");
-		
-		nota = 100;
-		quociente = resto / (nota * 100);
-		System.out.println(quociente + " nota(s) de R$ " + nota + ".00");
-		resto = resto % (nota * 100);
-		
-		nota = 50;
-		quociente = resto / (nota * 100);
-		System.out.println(quociente + " nota(s) de R$ " + nota + ".00");
-		resto = resto % (nota * 100);
-		
-		nota = 20;
-		quociente = resto / (nota * 100);
-		System.out.println(quociente + " nota(s) de R$ " + nota + ".00");
-		resto = resto % (nota * 100);
-		
-		nota = 10;
-		quociente = resto / (nota * 100);
-		System.out.println(quociente + " nota(s) de R$ " + nota + ".00");
-		resto = resto % (nota * 100);
-		
-		nota = 5;
-		quociente = resto / (nota * 100);
-		System.out.println(quociente + " nota(s) de R$ " + nota + ".00");
-		resto = resto % (nota * 100);     
-		
-		nota = 2;
-		quociente = resto / (nota * 100);
-		System.out.println(quociente + " nota(s) de R$ " + nota + ".00");
-		resto = resto % (nota * 100);
-		
-		
-		System.out.println("MOEDAS:");
-		moeda = 100;
-		quociente = resto / moeda;
-		System.out.println(quociente + " moeda(s) de R$ 1.00");
-		resto = resto % moeda;
+		Double valorDouble =  leitor.nextDouble();
+		System.out.println(df.format(valorDouble));
 
-		moeda = 50;
-		quociente = resto / moeda;
-		System.out.println(quociente + " moeda(s) de R$ 0.50");
-		resto = resto % moeda;
-		
-        moeda = 25;
-		quociente = resto / moeda;
-		System.out.println(quociente + " moeda(s) de R$ 0.25");
-		resto = resto % moeda;
-
-        moeda = 10;
-		quociente = resto / moeda;
-		System.out.println(quociente + " moeda(s) de R$ 0.10");
-		resto = resto % moeda;
-
-        moeda = 5;
-		quociente = resto / moeda;
-		System.out.println(quociente + " moeda(s) de R$ 0.05");
-		resto = resto % moeda;
-
-        moeda = 1;
-		quociente = resto / moeda;
-		System.out.println(quociente + " moeda(s) de R$ 0.01");
-		resto = resto % moeda;
-
-
-		
-
-		sc.close();
+		leitor.close();
 	}
 }
 
